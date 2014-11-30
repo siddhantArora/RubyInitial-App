9c80d493ca2ae6c2ee6a23af6be6b4acfa19b7c4
siddhant arora <siddhantarora@siddhants-MacBook-Pro.local>
2014-11-30 12:21:50 +0530

    authorization modified

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,19 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
+  # now user has to sign in ,otherwise they cant create a new pin , since there are too many in "only"
+  #so better use "except"
+
+  #before_action :authenticate_user!,only:[:new,:edit,:create]
+
+
+    #since we can access and edit the pin which doesnot belong to the current log in user
+    #We ahve to stop the access of the  edit of that pin
+  before_action :correct_user,only:[:edit ,:update,:destroy]
+
+  #use has to authenicate on all pages excet the index and user
+  before_action :authenticate_user!,except:[:index,:show]
+
   respond_to :html
 
   def index
@@ -13,7 +26,8 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+   # @pin = Pin.new
+   @pin = current_user.pins.build
     respond_with(@pin)
   end
 
@@ -21,7 +35,8 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    #@pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     @pin.save
     respond_with(@pin)
   end
@@ -39,6 +54,14 @@ class PinsController < ApplicationController
   private
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    #since we can access and edit the pin which doesnot belong to the current log in user
+    #We ahve to stop the access of the  edit of that pin
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin "if@pin.nil?
     end
 
     def pin_params
