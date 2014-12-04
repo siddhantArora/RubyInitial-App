4a882d5117208c5150ff92ba7bec0060908259d9
siddhant arora <siddhantarora@siddhants-MacBook-Pro.local>
2014-12-04 12:33:11 +0530

    amazon s3 support added to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
   #required for Heroku
   #NOTE : Set he host to actual host of your server 
   config.action_mailer.default_url_options = { host: 'http://siddhant-ruby.herokuapp.com'}
+
+  #support for papaerclip images to amazon s3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
